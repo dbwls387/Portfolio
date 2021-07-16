7d3c337b35b7de5b1dea69906242006404eb30fb
dbwls387 <dbwls387@naver.com>
2021-07-16 20:07:56 +0900

    JS: home cantact btn

--- main.js
+++ main.js
@@ -24,7 +24,16 @@ navbarMenu.addEventListener('click', (event) => {
     return; 
   }
 
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link); 
-  scrollTo.scrollIntoView({behavior: 'smooth'});
+  scrollIntoView(link);
 });
+
+// "contact me" 클릭 시 컨택트 미로 스크롤링 
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector); 
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+}
