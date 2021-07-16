d4b23ccc2fb36ff5c9e34967b5c5ec9978f8a68e
dbwls387 <dbwls387@naver.com>
2021-07-16 19:59:08 +0900

    JS: navbar menu click

--- main.js
+++ main.js
@@ -14,3 +14,17 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+// navbar의 메뉴를 선택했을 시 스크롤링 해주기 
+const navbarMenu = document.querySelector('.navbar__menu'); 
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if(link == null){ // navbar의 빈 공간을 누를 경우 
+    return; 
+  }
+
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link); 
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+});
